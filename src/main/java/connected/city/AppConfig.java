3b9c44a75228e044b3fdf2a7038f1a7df06f34ea
Tarun <tarungupta1234@gmail.com>
2020-07-31 17:02:12 -0400

    Added description in swagger

--- src/main/java/connected/city/AppConfig.java
+++ src/main/java/connected/city/AppConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -23,6 +25,14 @@ public class AppConfig {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("connected.city")).build();
+				.apis(RequestHandlerSelectors.basePackage("connected.city")).build().apiInfo(apiInfo());
 	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Spring Boot Connected City API")
+				.description("Tell us city name, we'll let you know if conenction is available").version("1.0.0")
+				.build();
+
+	}
+
 }
--- src/main/java/connected/city/CityController.java
+++ src/main/java/connected/city/CityController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -15,6 +16,7 @@ public class CityController {
 	CityConnection connection;
 
 	@GetMapping("/connected")
+	@ApiOperation("Checks if any connection is available between two cities or not.")
 	public String isConnected(@RequestParam String city1, @RequestParam String city2) {
 		log.debug("Checking route between: ", city1, " and ", city2);
 		if (city1 != null && city2 != null && connection.isConnectionAvailable(city1, city2)) {
